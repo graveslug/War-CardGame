f6c7a6a8fa0204476d0f141bf511f076478de6e9
Philip Gray <pcgrayiii@icloud.com>
2020-07-31 15:22:16 -0700

    pseudo code & function setup

---
+++ main/main.js
@@ -0,0 +1,83 @@
+
+//Default array: an array that holds 52 numbers that contains 4 repeats of each number from 1-13
+const deck = [1,1,1,1,
+                2,2,2,2,
+                3,3,3,3,
+                4,4,4,4,
+                5,5,5,5,
+                6,6,6,6,
+                7,7,7,7,
+                8,8,8,8,
+                9,9,9,9,
+                10,10,10,10,
+                11,11,11,11,
+                12,12,12,12,
+                13,13,13,13
+            ]
+
+    //computer array
+    const computer = []
+    //player array
+    const player = []
+
+        //startgame function//
+    const startGame = () => {
+        //shuffleDeck()
+    }
+
+//a function that manipulates the array by randomizing it.
+const shuffleDeck = () => {
+    //shuffleArray
+
+    //calls divideCards()
+}
+
+
+const divideCards = () => {
+    //then splits array in half
+    //inputs first half into computer array
+    //inputs second half into player array
+}
+
+
+
+//then these numbers are compared and the highest number wins thus both numbers pushed onto the end of the winners array.
+
+
+const doubleNumber = () => {
+    //when two numbers are equal then another number is drawn
+    //pulls one more number from the arrays
+    //compares them
+    //if double then pull another
+    //otherwise the highest number wins.
+    //pushes all numbers to winners array
+}
+
+
+
+//play card FUNCTION
+const playCard = () => {
+    //on each turn when a player selects the play button or maybe a card click later the first number of the array is 'played'in the computer array and the player array.
+
+
+}
+
+
+//current card function
+const currentCardCounter = () =>{
+    //computer array length counted then display
+    //player array length counted then display
+}
+
+
+//win condition: when one array reaches 52.
+const checkWin = () => {
+    //if player === 52 && computer === 0
+    //YOU WIN!
+    //else if player === 0 && computer === 52
+    //YOU LOSE
+    //probaly should make this more dynamic. Later.
+}
+
+
+//startGame()
